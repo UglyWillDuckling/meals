453ed3394d1347b8b963bee3d164dfbbfe1c0a91
Vladimir Sedlar <vladimir.sedlar@gauss.hr>
2017-08-07 19:31:33 +0200

    added all filters to query

--- app/Http/Controllers/EndpointController.php
+++ app/Http/Controllers/EndpointController.php
@@ -23,32 +23,8 @@ class EndpointController extends Controller
         $meals = $this->getMeals();
 
         dd($meals->toArray());
-    }
 
-    /**
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    private function generateQuery()
-    {
-        $request = $this->request;
-        $query = Meal::query();
-
-        $query
-            ->join('meals_translation as mt', function (\Illuminate\Database\Query\JoinClause $join) use($request) {
-                $join->on('mt.meal_id', '=', 'meals.id');
-                $join->where(
-                    'mt.language_id', '=', $request->lang);
-             });
-
-        if ($request->tags) {
-            $tags = $this->getTags();
-            $query
-                ->join('meals_tags as mtgs', function (\Illuminate\Database\Query\JoinClause $join) use ($tags) {
-                    $join->on('mtgs.meal_id', '=', 'meals.id');
-                    $join->whereIn('mtgs.tag_id', $tags);
-                });
-        }
-        return $query;
+//        dd(DB::getQueryLog());
     }
 
     /**
@@ -67,9 +43,50 @@ class EndpointController extends Controller
     }
 
     /**
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    private function generateQuery()
+    {
+        $request = $this->request;
+        $query = Meal::query();
+
+        $query
+            ->join('meals_translation as mt', function (\Illuminate\Database\Query\JoinClause $join) use ($request) {
+                $join->on(
+                    'mt.meal_id', '=', 'meals.id');
+                $join->where(
+                    'mt.language_id', '=', $request->lang);
+            });
+
+        if ($request->tags) {
+            $tags = $this->getTags();
+            $query
+                ->join('meals_tags as mtgs', function (\Illuminate\Database\Query\JoinClause $join) use ($tags) {
+                    $join->on(
+                        'mtgs.meal_id', '=', 'meals.id');
+                    $join->whereIn(
+                        'mtgs.tag_id', $tags);
+                });
+        }
+        if ($request->category) {
+            $query->where('category_id', '=', $request->category);
+        }
+        if ($request->diff_time) {
+            $query->where(
+                'meals.updated_at', '>=', date("Y-m-d h:i:s", strtotime($request->diff_time)
+            ));
+        } else {
+            $query->where('status', '=', 1);
+        }
+
+        return $query;
+    }
+
+    /**
      * @return array
      */
-    private function getTags(){
+    private function getTags()
+    {
         return explode(',', $this->request->tags);
     }
 
